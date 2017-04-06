3caa57dd6f48acf5ca570959169b4d905f0bf73b
sertony <serkov_anton@mail.ru>
2017-04-06 14:10:22 +0300

    resolve conflicts

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -292,22 +292,11 @@ class GiftedChat extends React.Component {
       return;
     }
     this.setMaxHeight(layout.height);
-<<<<<<< HEAD
-    GiftedChatInteractionManager.runAfterInteractions(() => {
-      this.setState({
-        isInitialized: true,
-        text: '',
-        composerHeight: MIN_COMPOSER_HEIGHT,
-        messagesContainerHeight: this.prepareMessagesContainerHeight(this.getMaxHeight() - this.getMinInputToolbarHeight()),
-      });
-=======
-
     this.setState({
       isInitialized: true,
       text: '',
       composerHeight: MIN_COMPOSER_HEIGHT,
       messagesContainerHeight: this.prepareMessagesContainerHeight(this.getMaxHeight() - this.getMinInputToolbarHeight()),
->>>>>>> origin/web
     });
   }
 
